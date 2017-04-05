178787b103b08537c38119274d11e9dbfadd90ae
github username <github email address>
2017-04-05 16:41:20 -0400

    team: @gabystern

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,26 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
+  # def brand_array(brand)
+  #   BRANDS << brand
+  # end
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
+
+
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) == false
+      BRANDS << brand
+    end
   end
 
   def cobble
